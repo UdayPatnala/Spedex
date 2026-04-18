9d6796ba383667ba46789698c79120cb8e3d1365
Patnala Uday Kumar <udaypatnala5@gmail.com>
2026-04-18 14:35:17 +0530

    Fix compilation: add missing CrossOrigin imports in controllers

--- backend/src/main/java/com/spedex/controller/DashboardController.java
+++ backend/src/main/java/com/spedex/controller/DashboardController.java
@@ -4,9 +4,7 @@ import com.spedex.service.DashboardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
